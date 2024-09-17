665ddd6bd653f5936fd814aa8af7afe244258203
gabrielimpateco <al.gabriel.vieira@impatech.org.br>
2024-09-17 15:52:30 -0300

    Update updatelista1.py

--- updatelista1.py
+++ updatelista1.py
@@ -21,87 +21,27 @@ class Field:
     pass
 
 class VectorSpace:
-    """VectorSpace:
-    Abstract Class of vector space used to model basic linear structures
-    """
     
     def __init__(self, dim: int, field: 'Field'):
-        """
-        Initialize a VectorSpace instance.
-
-        Args:
-            dim (int): Dimension of the vector space.
-            field (Field): The field over which the vector space is defined.
-        """
         self.dim = dim
         self._field = field
         
     def getField(self):
-        """
-        Get the field associated with this vector space.
-
-        Returns:
-            Field: The field associated with the vector space.
-        """
         return self._field
     
     def getVectorSpace(self):
-        """
-        Get a string representation of the vector space.
-
-        Returns:
-            str: A string representing the vector space.
-        """
         return f'dim = {self.dim!r}, field = {self._field!r}'
-        # return self.__repr__()
 
     def __repr__(self):
-        """
-        Get a string representation of the VectorSpace instance.
-
-        Returns:
-            str: A string representing the VectorSpace instance.
-        """
-        # return f'dim = {self.dim!r}, field = {self._field!r}'
         return self.getVectorSpace()
     
     def __mul__(self, f):
-        """
-        Multiplication operation on the vector space (not implemented).
-
-        Args:
-            f: The factor for multiplication.
-
-        Raises:
-            NotImplementedError: This method is meant to be overridden by subclasses.
-        """
         raise NotImplementedError
     
     def __rmul__(self, f):
-        """
-        Right multiplication operation on the vector space (not implemented).
-
-        Args:
-            f: The factor for multiplication.
-
-        Returns:
-            The result of multiplication.
-
-        Note:
-            This method is defined in terms of __mul__.
-        """
         return self.__mul__(f)
     
     def __add__(self, v):
-        """
-        Addition operation on the vector space (not implemented).
-
-        Args:
-            v: The vector to be added.
-
-        Raises:
-            NotImplementedError: This method is meant to be overridden by subclasses.
-        """
         raise NotImplementedError
     
 #-----------------------------
@@ -226,7 +166,7 @@ class Vector3D(RealVector):
 
     @staticmethod
     def _builder(coord):
-        return Vector2D(coord)
+        return Vector3D(coord)
     
     def CW(self):
         return Vector2D([-self.coord[1], self.coord[0]])
@@ -235,19 +175,58 @@ class Vector3D(RealVector):
     def CCW(self):
         return Vector2D([self.coord[1], -self.coord[0]])    
     
+'''
+O polinomio vai ser escrito com as potencias crescendo da esquerda para a direita,
+por exemplo, o polinomio 1 - 6x^1 + 3x^2 é escrito como [1, -6, 3]
+'''   
+    
+class Polinomio(RealVector):
 
+    def __init__(self, coord):
+        super().__init__(len(coord), coord)
 
+    def __add__(self, other_vector):
+        return super().__add__(other_vector)
+
+    def __sub__(self, other_vector):
+        return super().__sub__(other_vector)
+    
+    def __mul__(self, alpha):
+        return super().__mul__(alpha)
+
+    def __str__(self):
+        poli = ''
+        for i in range(len(self.coord)):
+            if self.coord[i] == 0:
+                continue
+            if i == 0:
+                poli += f'{self.coord[i]} '
+                continue
+            if self.coord[i] > 0:
+                poli += f'+ {self.coord[i]}x^{i} '  
+            else:
+                poli += f'- {abs(self.coord[i])}x^{i} '  
+        if poli[0] == '+' or poli[0:1] == '- ':
+            poli = poli[2:]
+        return poli[:-1]
+    
+    @staticmethod
+    def _builder(coord):
+        return Polinomio(coord)
 
 if __name__ == '__main__':
-    V2 = Vector2D([1, 2])
-    print('V2 = ', V2)
-    W2 = Vector2D([3, 4])
-    print('W2 = ', W2)
-
-    print(V2.getVectorSpace())
-
-    r = V2+4*W2
-    print('V2 + 4*W2 =', r)
-    print('(V2 + 4*W2).CW() = ', r.CW())
-    print('W2.CCW() = ', W2.CCW())
-    print('V2.iner_prod(W2) = ', V2.iner_prod(W2))
+    print('Vetores 3D')
+    vetor3d1 = Vector3D([2, 4, 8])
+    print('V1: ' , vetor3d1)
+    vetor3d2 = Vector3D([7, 3, 5])
+    print('V2: ' , vetor3d2)
+    print('V1 + V2: ', vetor3d2 + vetor3d1)
+    print('V1 - V2: ', vetor3d1 - vetor3d2)
+    print('-V1: ', vetor3d1.negative())
+    print('\nPolinomios')
+    poli1 = Polinomio([2, 5, 1])
+    print('P1: ', poli1)
+    poli2 = Polinomio([4, 2, 7])
+    print('P2: ', poli2)
+    print('P1 + P2: ', poli1 + poli2)
+    print('P1 - P2: ', poli1 - poli2)
